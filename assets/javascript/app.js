0f57a35e936af9e0438dd41463c3b3b783e4edb4
emssslay <emily.robinson@iechs.org>
2019-03-06 16:32:48 -0700

    the beginning

---
+++ assets/javascript/app.js
@@ -0,0 +1,47 @@
+// Global Variables 
+//=============================================================
+var questions =[{
+    question: ;
+    answers:[];
+    correctAnswer: "";
+}, {
+    question: ;
+    answers:[];
+    correctAnswer: "";
+}, {
+    question: ;
+    answers:[];
+    correctAnswer: "";
+}, {
+    question: ;
+    answers:[];
+    correctAnswer: "";
+}, {
+    question: ;
+    answers:[];
+    correctAnswer: "";
+}, {
+    question: ;
+    answers:[];
+    correctAnswer: "";
+}]
+
+// Functions
+//=============================================================
+
+// create an on click function for the start button 
+$('#start-button').on('click', function() {
+
+});
+
+
+// create an array with the questions as objects, answers[], and their correct answers
+
+
+
+
+// create if/else statements for each question. If their answer is === to the correctAnswer, else it is wrong
+
+
+// Main process 
+//=============================================================
